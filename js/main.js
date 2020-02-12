1ab02770a0aa86df8b5ed432db0fbbb35da896e2
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-02-12 22:37:52 +0300

    add web site

---
+++ js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const changeCheck = item => {
+   const checkbox = document.querySelector(`.checkbox${item}`);
+   const content = document.querySelectorAll(`.tumbl${item}`);
+
+   if (checkbox.checked) {
+      for (let i = 0; i <= content.length - 1; i++) {
+         if (content[i].getAttribute("tumbl") === "on") {
+            content[i].style.display = "inline-block";
+         }
+
+         if (content[i].getAttribute("tumbl") === "off") {
+            content[i].style.display = "none";
+         }
+      }
+   } else {
+      for (let i = 0; i < content.length; i++) {
+         if (content[i].getAttribute("tumbl") === "on") {
+            content[i].style.display = "none";
+         }
+
+         if (content[i].getAttribute("tumbl") === "off") {
+            content[i].style.display = "inline-block";
+         }
+      }
+   }
+};
